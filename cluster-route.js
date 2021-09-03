8959102be7727532f45db698a92e31ffcfaa3862
spacepirate95 <52543574+spacepirate95@users.noreply.github.com>
2021-09-03 23:21:13 +0530

    Update cluster-route.js

--- cluster-route.js
+++ cluster-route.js
@@ -12,11 +12,22 @@ var util = require('./util');
 
 
 
-
-
-
-
-
-
-
+/*Create cluster */
+cluster.post('/', async function(req, res, next) {
+    var clusterJson = req.body;
+    var requester = util.getUser(req);
+    try{
+        let clusterCreated = await clusterHelper.create(clusterJson,requester);
+        console.log(clusterCreated);
+        if(clusterCreated){
+            responder.respond(res,clusterCreated,responder.SUCCESS,"Cluster created successfully");
+        }
+        else{
+            responder.respond(res,null,responder.FAILED,"Cluster creation failed");
+        }
+    }catch(fault){
+        logger.error("Cluster creation failed. Reason:",fault);
+        responder.respond(res,null,responder.FAILED,"Cluster creation failed. Reason:"+fault);
+    }
+});
 
